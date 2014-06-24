a95a70616903e8fcbd0ce42f7d1b5160836118e5
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-06-24 08:58:01 -0400

    add more sprints

--- ffos/js/Dimension-Features.js
+++ ffos/js/Dimension-Features.js
@@ -85,21 +85,6 @@ Dimension.addEdges(true, Mozilla, [
 			]
 		},
 		{
-			"name": "Firefox32",
-			"start_date": "29 APR 2014",
-			"targetDate": "9 JUN 2014",
-			"partitions": [
-				{"name": "Blocking", "esfilter": {"and": [
-					{"term": {"cf_blocking_loop": "fx32+"}}
-				]}},
-				{"name": "Targeted", "esfilter": {"and": [
-					{"term": {"target_milestone": "mozilla32"}},
-					{"not": {"term": {"cf_blocking_loop": "fx32+"}}}  // UNFORTUNATE REDUNDANCY
-				]}}
-			]
-		},
-
-		{
 			"name": "2.0 S1",
 			"start_date": "28 APR 2014",
 			"targetDate":"9 MAY 2014",   //END OF DAY
@@ -121,6 +106,88 @@ Dimension.addEdges(true, Mozilla, [
 			"start_date": "12 MAY 2014",
 			"targetDate":"23 MAY 2014",
 			"esfilter": {"term":{"target_milestone":"2.0 S2 (23may)"}},
+			"partitions": [
+				{"name": "Blocking", "esfilter": {"and": [
+					{"terms": {"cf_blocking_b2g": ["2.0+", "1.5+"]}}
+				]}},
+				{"name": "Targeted", "esfilter": {"and": [
+					{"exists": {"field": "target_milestone"}},
+					{"not": {"term": {"target_milestone": "---"}}}
+				]}}
+
+			]
+		},
+
+		{
+			"name": "2.0 S3",
+			"start_date": "26 MAY 2014",
+			"targetDate": "6 JUNE 2014",
+			"esfilter": {"term":{"target_milestone":"2.0 S3 (6june)"}},
+			"partitions": [
+				{"name": "Blocking", "esfilter": {"and": [
+					{"terms": {"cf_blocking_b2g": ["2.0+", "1.5+"]}}
+				]}},
+				{"name": "Targeted", "esfilter": {"and": [
+					{"exists": {"field": "target_milestone"}},
+					{"not": {"term": {"target_milestone": "---"}}}
+				]}}
+
+			]
+		},
+		{
+			"name": "Firefox32",
+			"start_date": "29 APR 2014",
+			"targetDate": "9 JUN 2014",
+			"partitions": [
+				{"name": "Blocking", "esfilter": {"and": [
+					{"term": {"cf_blocking_loop": "fx32+"}}
+				]}},
+				{"name": "Targeted", "esfilter": {"and": [
+					{"term": {"target_milestone": "mozilla32"}},
+					{"not": {"term": {"cf_blocking_loop": "fx32+"}}}  // UNFORTUNATE REDUNDANCY
+				]}}
+			]
+		},
+
+		{
+			"name": "2.0 S4",
+			"start_date": "9 JUN 2014",
+			"targetDate":"20 JUN 2014",
+			"esfilter": {"term":{"target_milestone":"2.0 S4 (20june)"}},
+			"partitions": [
+				{"name": "Blocking", "esfilter": {"and": [
+					{"terms": {"cf_blocking_b2g": ["2.0+", "1.5+"]}}
+				]}},
+				{"name": "Targeted", "esfilter": {"and": [
+					{"exists": {"field": "target_milestone"}},
+					{"not": {"term": {"target_milestone": "---"}}}
+				]}}
+
+			]
+		},
+
+		{
+			"name": "2.0 S5",
+			"start_date": "23 JUN 2014",
+			"targetDate":"4 JULY 2014",
+			"esfilter": {"term":{"target_milestone":"2.0 S5 (4july)"}},
+			"partitions": [
+				{"name": "Blocking", "esfilter": {"and": [
+					{"terms": {"cf_blocking_b2g": ["2.0+", "1.5+"]}}
+				]}},
+				{"name": "Targeted", "esfilter": {"and": [
+					{"exists": {"field": "target_milestone"}},
+					{"not": {"term": {"target_milestone": "---"}}}
+				]}}
+
+			]
+		},
+
+		{
+			"name": "2.0 S6",
+			"start_date": "7 JULY 2014",
+			"targetDate":"18 JULY 2014",
+			"esfilter": {"term":{"target_milestone":"2.0 S6 (18july)"}},
 			"partitions": [
 				{"name": "Blocking", "esfilter": {"and": [
 					{"terms": {"cf_blocking_b2g": ["2.0+", "1.5+"]}}
